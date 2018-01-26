24298b7a70158b746a47fa6f6c24cf4e4681ca67
John Doe <jdgblackfox@gmail.com>
2018-01-25 19:00:17 -0500

    Finds primes. Don't use for large collections of high numbers. (i.e. don't try to find all primes in the range of 30,000-50,000). Didn't use the math library at all.

--- primes.py
+++ primes.py
@@ -3,12 +3,30 @@
 import math as ma
 # To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
 
+print("This program will find prime numbers. Choose two positive integers to find primes between: ")   
 # These functions will ask you for your number ranges, and assign them to 'x1' and 'x2'
-x1 = raw_input('smallest number to check: ')
-x2 = raw_input('largest number to check: ')
+x1 = int(raw_input('Smallest number to check: '))
+x2 = int(raw_input('Largest number to check: '))
+if x1<0 or x2<0:
+    print("Please input positive integers") #For catching errors at the beginning.
+else:
+    primeslist=[]
+    for counter in range(x1,x2+1):
+        checker=True
+        for number in range (2,counter):#Doesn't include 1 or the number itself, since those wouldn't count in searching for primes
+            if float(counter)/number==counter/number:
+                checker=False
+            if counter==1:
+                checker=False
+        if checker==True:
+            primeslist.append(counter)
+    print "Finding primes between",x1,"and", x2
+    print ("...")
+    for walker in range(1,len(primeslist)+1):
+        print(primeslist[walker-1]) #This seems like a better/more readable format than just printing the list
+    if len(primeslist)==0:
+        print("No primes found")
 
-# This line will print your two numbers out
-print x1, x2
 
 """ Text enclosed in triple quotes is also a comment.
 This code should find and output all prime numbers between (and including) the numbers entered initially.
