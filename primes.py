26f7594d5c77d9bc6fda2d614e6d5366e0acd1b6
John Doe <jdgblackfox@gmail.com>
2018-01-30 17:52:02 -0500

    Better version. Runs faster, more error catching

--- primes.py
+++ primes.py
@@ -7,6 +7,14 @@ print("This program will find prime numbers. Choose two positive integers to fin
 # These functions will ask you for your number ranges, and assign them to 'x1' and 'x2'
 x1 = int(raw_input('Smallest number to check: '))
 x2 = int(raw_input('Largest number to check: '))
+if x2<x1:
+    two=x2
+    one=x1
+    x2=one
+    x1=two
+    print("Your first number was higher than the second. I reversed those for you")
+
+
 if x1<0 or x2<0:
     print("Please input positive integers") #For catching errors at the beginning.
 else:
@@ -20,13 +28,18 @@ else:
                 checker=False
         if checker==True:
             primeslist.append(counter)
-    print "Finding primes between",x1,"and", x2
+    print "Attempting to find primes between",x1,"and", x2
     print ("...")
     for walker in range(1,len(primeslist)+1):
         print(primeslist[walker-1])#This seems like a better/more readable format than just printing the list
     if len(primeslist)==0:
         print("No primes found")
-print("There are "+str(len(primeslist))+" primes between "+str(x1)+" and "+str(x2))
+if x2==x1:
+    print("You put in the same number twice. There are zero primes between that number and itself.")
+    if len(primeslist)>0:
+        print ("However, that number is itself a prime.")
+else:
+    print("There are "+str(len(primeslist))+" primes between "+str(x1)+" and "+str(x2))
 
 
 """ Text enclosed in triple quotes is also a comment.
